1b48feb3bfefa95b228a369318d4ad765f9352d7
Miguel Leon <miguel.e.leon.f@gmail.com>
2023-11-20 23:30:42 -0300

    🚧 move by position instead of velocity and add a spring-like behavior to player to return to floor.

--- Assets/Scripts/BoundaryController.cs
+++ Assets/Scripts/BoundaryController.cs
@@ -1,27 +1,29 @@
+using System;
 using UnityEngine;
 
-public class BoundaryController : MonoBehaviour
-{
-    public GameObject ground;
 
-    private Vector2 _boundaryMin;
-    private Vector2 _boundaryMax;
+public class BoundaryController : MonoBehaviour {
+	public GameObject ground;
 
-    private Rigidbody2D _rigidBody;
-    private PolygonCollider2D _currentFloor;
+	private Vector2 _boundaryMin;
+	private Vector2 _boundaryMax;
 
-    void Start()
-    {
-        _currentFloor = ground.GetComponent<PolygonCollider2D>();
-        _rigidBody = GetComponent<Rigidbody2D>();
-    }
+	private Rigidbody2D _rigidBody;
+	private PolygonCollider2D _currentFloor;
+	private Move _moveBehavior;
 
-    void Update()
-    {
-    }
-    
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        _rigidBody.position = collision.ClosestPoint(_rigidBody.position);
-    }
+	void Start() {
+		_currentFloor = ground.GetComponent<PolygonCollider2D>();
+		_rigidBody = GetComponent<Rigidbody2D>();
+		_moveBehavior = GetComponent<Move>();
+	}
+
+	void OnTriggerExit2D(Collider2D floor) {
+		_moveBehavior.inFloor = false;
+		_moveBehavior.closestPoint = floor.ClosestPoint(_rigidBody.position);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		_moveBehavior.inFloor = true;
+	}
 }
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
-{
-    [SerializeField] float hMaxSpeed;
-    [SerializeField] float vMaxSpeed;
-    float vertical, horizontal;
-    Rigidbody2D myRigidBody2D;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        myRigidBody2D = GetComponent<Rigidbody2D>();
-    }
+public class Move : MonoBehaviour {
+	[SerializeField] float hMaxSpeed;
+	[SerializeField] float vMaxSpeed;
+	float _vertical, _horizontal;
+	Rigidbody2D _rigidBody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        movePlayer();
-    }
+	public bool inFloor;
+	public Vector2 closestPoint;
 
-    void movePlayer()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        myRigidBody2D.velocity = new Vector2(horizontal * hMaxSpeed, vertical * vMaxSpeed);
-    }
+	void Start() {
+		_rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		MovePlayer();
+	}
+
+	void MovePlayer() {
+		_horizontal = Input.GetAxisRaw("Horizontal");
+		_vertical = Input.GetAxisRaw("Vertical");
+
+		var attemptedPosition = _rigidBody.position + new Vector2(_horizontal * hMaxSpeed, _vertical * vMaxSpeed) * Time.deltaTime;
+
+		if (inFloor) {
+			_rigidBody.position = attemptedPosition;
+		}
+		else {
+			_rigidBody.position = Vector2.Lerp(attemptedPosition, closestPoint, 0.01f);
+		}
+	}
 }
